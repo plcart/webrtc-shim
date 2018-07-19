f6b552a332fff2a512c49c71523443d3d5ff8cf1
Arthur Ferreira <Arthur.Ferreira@fleetmatics.com>
2018-07-19 13:27:25 +0100

    connectionState change

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ export class WebRTCShim {
         }
 
         pc.oniceconnectionstatechange = e => {
-            if (this.peerConnection && this.peerConnection.iceConnectionState == 'disconnected') {
+            if (this.peerConnection && this.peerConnection.connectionState == 'disconnected') {
                 this.hangup();
             }
         }
